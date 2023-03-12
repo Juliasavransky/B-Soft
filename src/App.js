59646fdd05b93ee622493f0ef11d04d5087ceaa5
Juliasavransky <63636407+Juliasavransky@users.noreply.github.com>
2023-03-12 23:44:15 +0200

    stars

--- src/App.js
+++ src/App.js
@@ -18,19 +18,19 @@ function App() {
           <div className="br--review__title">
             כמה מילים על החוויה שלך עם העסק
           </div>
-          <input textarea className="br--review__input" type="text" placeholder="יש להקליד כאן..." />
+          <input textarea="true" className="br--review__input" type="text" placeholder="יש להקליד כאן..." />
         </div>
         <div className="divider-bottom"></div>
 
         <div className="br--photo">
           <input id="img" className="br--photo__input" type="file"></input>
-          <label for="img">הוספת תמונה</label>
+          <label htmlFor="img">הוספת תמונה</label>
         </div>
         <div className="divider-bottom"></div>
 
         <div className="br--confirmation">
           <input type="checkbox" id="box" className="br--confirmation__box"></input>
-          <label for="box" className="br--confirmation">קראתי את  <a href="https://www.b144.co.il/TermsOfService/">תנאי השימוש  </a>והם מקובלים עלי</label>
+          <label htmlFor="box" className="br--confirmation">קראתי את  <a href="https://www.b144.co.il/TermsOfService/">תנאי השימוש  </a>והם מקובלים עלי</label>
 
           <button>שיתוף תגובה</button>
         </div>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,25 +1,69 @@
 import React from 'react'
 import Star from '../img/star-empty-darkBlue.svg'
 
+const ratingArray = [
+    {
+        text: "שירות מקצועי ",
+        dataRate: "5",
+        src: Star,
+        starNum: [1, 2, 3, 4, 5]
+    },
+    {
+        text: " מחיר ממוצע ",
+        dataRate: "4",
+        src: Star,
+        starNum: [1, 2, 3, 4, 5]
+
+    }, {
+        text: "  מקצועיות הצוות ",
+        dataRate: "3",
+        src: Star,
+        starNum: [1, 2, 3, 4, 5]
+
+    }, {
+        text: " זמינות בעל העסק  ",
+        dataRate: "2",
+        src: Star,
+        starNum: [1, 2, 3, 4, 5]
+
+    }, {
+        text: " התרשמות כללית ",
+        dataRate: "1",
+        src: Star,
+        starNum: [1, 2, 3, 4, 5]
+
+    },
+]
 function Rating() {
     return (
         <div className="br--rating">
             <div className="br--rating__title">עד כמה היית מרוצה מהעסק?</div>
 
             <div>
-                <div className="br--rating__category" >שירות מקצועי  </div>
-                <img src={Star} alt="rating star" />
+                {ratingArray.map((rating, index) => (
+                    <div key={index} className="br--rating__category">
+                        <div data-rate={rating.dataRate}>  {rating.text} </div>
+                        {rating.starNum.map((num) => (
+                            <div key={num} className=".br--rating__star" >
+                                <img src={rating.src} alt="rating star" />
+                                <div>{num}</div>
+                            </div>
+                        ))
+                        }
+
+
+                    </div>
+
+                ))}
+
+                {/* <img src={rating.src} alt="rating star" />{index + 1}
+                        <img src={rating.src} alt="rating star" />{index + 1}
+                        <img src={rating.src} alt="rating star" />{index + 1}
+                        <img src={rating.src} alt="rating star" />{index + 1}
+                        <img src={rating.src} alt="rating star" />{index + 1} */}
             </div>
 
-
-
-
-            {/* <div className="br--rating__category"> שירות מקצועי </div>
-            <div className="br--rating__category">מחיר ממוצע</div>
-            <div className="br--rating__category"> מקצועיות הצוות</div>
-            <div className="br--rating__category">זמינות בעל העסק </div>
-            <div className="br--rating__category">התרשמות כללית</div> */}
-        </div>
+        </div >
     )
 }
 
